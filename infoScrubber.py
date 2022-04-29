7e33982c6dd392145eb6c01b602cb37891f37869
vidalHusky <104016058+vidalHusky@users.noreply.github.com>
2022-04-28 22:09:58 -0400

    Update infoScrubber.py

--- infoScrubber.py
+++ infoScrubber.py
@@ -2,35 +2,84 @@ from movie import Movie
 
 
 def suggest_based_on_genre(genre, movie_library):
-    lst_of_chosen_movies = []
-    for mov in movie_library.values():
-        if genre in str(mov.get_genre()):
-            lst_of_chosen_movies.append(mov.__str__())
-    return lst_of_chosen_movies
+    """
+    :param genre: the designated genre of movie wanted
+    :param movie_library: the library of all wanted movies
+    :return: the library of all wanted movies, according to genre
+    """
+    library_of_chosen_movies = {}
+    for mov, mov_info in movie_library.items():
+        if genre in str(mov_info.get_genre()):
+            library_of_chosen_movies[mov] = mov_info
+    return library_of_chosen_movies
 
 
 def suggest_based_on_year(year, movie_library):
-    lst_of_chosen_movies = []
-    for mov in movie_library.values():
-        if year[0] <= mov.get_year() <= year[1]:
-            lst_of_chosen_movies.append(mov.__str())
-    return lst_of_chosen_movies
+    """
+    :param year: the designated year bounds of movie wanted
+    :param movie_library: the library of all wanted movies
+    :return: the library of all wanted movies according to year
+    """
+    library_of_chosen_movies = {}
+    for mov, mov_info in movie_library.items():
+        if year[0] <= mov_info.get_year() <= year[1]:
+            library_of_chosen_movies[mov] = mov_info
+    return library_of_chosen_movies
 
 
 def suggest_based_on_rating(rating, movie_library):
-    lst_of_chosen_movies = []
-    for mov in movie_library.values():
-        if rating in mov.get_rating():
-            lst_of_chosen_movies.append(mov.__str())
-    return lst_of_chosen_movies
+    """
+    :param rating: the designated rating of movie wanted
+    :param movie_library: the library of all wanted movies
+    :return: the library of all wanted movies according to their ratting
+    """
+    if rating == 'G':
+        library_of_chosen_movies = {}
+        for mov, mov_info in movie_library.items():
+            if 'PG' in mov_info.get_rating:
+                pass
+            elif rating in mov_info.get_rating():
+                library_of_chosen_movies[mov] = mov_info
+        return library_of_chosen_movies
+    if rating == 'PG':
+        library_of_chosen_movies = {}
+        for mov, mov_info in movie_library.items():
+            if 'PG-13' in mov_info.get_rating():
+                pass
+            elif rating in mov_info.get_rating():
+                library_of_chosen_movies[mov] = mov_info
+        return library_of_chosen_movies
+    if rating == 'R':
+        library_of_chosen_movies = {}
+        for mov, mov_info in movie_library.items():
+            if 'Not Rated' in mov_info.get_rating():
+                pass
+            elif rating in mov_info.get_rating():
+                library_of_chosen_movies[mov] = mov_info
+        return library_of_chosen_movies
+    else:
+        library_of_chosen_movies = {}
+        for mov, mov_info in movie_library.items():
+            if rating in mov_info.get_rating():
+                library_of_chosen_movies[mov] = mov_info
+        return library_of_chosen_movies
 
 
 def add_movie(movie_info):
+    """
+    Saves all important data of the movie into class movie
+    :param movie_info: the individual info for one movie
+    :return: Returns a variable mov thats basically a movie class that holds all the movies info
+    """
     mov = Movie(movie_info[0], movie_info[4:], movie_info[1], movie_info[3], movie_info[2])
     return mov
 
 
 def make_list_of_all_movie_info():
+    """
+    Reads in the text file created by the movie scrubber, and puts all movie data into a usable list
+    :return: Returns list of list where every nested list is a movie with all its relevant data
+    """
     big_lst = []
     with open('movieInfo.txt') as f:
         for line in f:
@@ -47,6 +96,9 @@ def make_list_of_all_movie_info():
 
 
 def make_dic_of_movies():
+    """
+    :return: returns a dictionary where the movie name is the key and the movie saved into a class is the value
+    """
     lst_of_movie_info = make_list_of_all_movie_info()
     dic_of_movies = {}
     for element in lst_of_movie_info:
